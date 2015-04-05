fc5352e24d637b98aa0838d18b20c55b564d5d63
sonjapeterson <sonja.peterson@gmail.com>
2015-04-05 15:28:23 -0400

    Add colors to minitest

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,7 @@
 require 'usatoday_bestsellers'
 require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
